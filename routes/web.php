6eeeaf402300a5d429753265868c1ba951dd614d
vagrant <vagrant@homestead>
2018-07-02 03:49:34 +0000

    用户界面 - 商品详情页

--- routes/web.php
+++ routes/web.php
@@ -33,4 +33,5 @@ Route::group(['middleware' => 'auth'], function(){
 
 Route::redirect('/', '/products')->name('root');
 Route::get('products', 'ProductsController@index')->name('products.index');
+Route::get('products/{product}', 'ProductsController@show')->name('products.show');
 
