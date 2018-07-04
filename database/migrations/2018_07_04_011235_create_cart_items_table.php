8a80c226d0369f373ab9e3dd2134fe2c11efaf92
vagrant <vagrant@homestead>
2018-07-04 02:41:22 +0000

    商品加入购物车

--- database/migrations/2018_07_04_011235_create_cart_items_table.php
+++ database/migrations/2018_07_04_011235_create_cart_items_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUserAddressesTable extends Migration
+class CreateCartItemsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,19 +13,13 @@ class CreateUserAddressesTable extends Migration
      */
     public function up()
     {
-        Schema::create('user_addresses', function (Blueprint $table) {
+        Schema::create('cart_items', function (Blueprint $table) {
             $table->increments('id');
             $table->unsignedInteger('user_id');
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
-            $table->string('province');
-            $table->string('city');
-            $table->string('district');
-            $table->string('address');
-            $table->unsignedInteger('zip');
-            $table->string('contact_name');
-            $table->string('contact_phone');
-            $table->dateTime('last_used_at')->nullable();
-            $table->timestamps();
+            $table->unsignedInteger('product_sku_id');
+            $table->foreign('product_sku_id')->references('id')->on('product_skus')->onDelete('cascade');
+            $table->unsignedInteger('amount');
         });
     }
 
@@ -36,6 +30,6 @@ class CreateUserAddressesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('user_addresses');
+        Schema::dropIfExists('cart_items');
     }
 }
