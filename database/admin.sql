b18acbcaaf9283518500cd99f27fd508a011f902
vagrant <vagrant@homestead>
2018-09-18 11:09:23 +0800

    商品类目管理后台

--- database/admin.sql
+++ database/admin.sql
@@ -1,6 +1,6 @@
 -- MySQL dump 10.13  Distrib 5.7.19, for Linux (x86_64)
 --
--- Host: localhost    Database: laravel-shop
+-- Host: 127.0.0.1    Database: laravel-shop
 -- ------------------------------------------------------
 -- Server version	5.7.19-0ubuntu0.16.04.1
 
@@ -21,7 +21,7 @@
 
 LOCK TABLES `admin_menu` WRITE;
 /*!40000 ALTER TABLE `admin_menu` DISABLE KEYS */;
-INSERT INTO `admin_menu` VALUES (1,0,1,'首页','fa-bar-chart','/',NULL,'2018-06-22 01:32:21'),(2,0,6,'系统管理','fa-tasks',NULL,NULL,'2018-08-10 01:28:05'),(3,2,7,'管理员','fa-users','auth/users',NULL,'2018-08-10 01:28:05'),(4,2,8,'角色','fa-user','auth/roles',NULL,'2018-08-10 01:28:05'),(5,2,9,'权限','fa-ban','auth/permissions',NULL,'2018-08-10 01:28:05'),(6,2,10,'菜单','fa-bars','auth/menu',NULL,'2018-08-10 01:28:05'),(7,2,11,'操作日志','fa-history','auth/logs',NULL,'2018-08-10 01:28:05'),(8,0,2,'用户管理','fa-user','/users','2018-06-22 01:57:13','2018-07-12 01:16:02'),(9,0,3,'商品管理','fa-cubes','/products','2018-06-22 06:12:42','2018-07-12 01:16:02'),(10,0,4,'订单管理','fa-rmb','/orders','2018-07-12 01:14:06','2018-07-12 01:16:02'),(11,0,5,'优惠券管理','fa-tags','/coupon_codes','2018-08-10 01:27:37','2018-08-10 01:28:05');
+INSERT INTO `admin_menu` VALUES (1,0,1,'首页','fa-bar-chart','/',NULL,'2018-06-22 01:32:21'),(2,0,7,'系统管理','fa-tasks',NULL,NULL,'2018-09-18 02:42:56'),(3,2,8,'管理员','fa-users','auth/users',NULL,'2018-09-18 02:42:56'),(4,2,9,'角色','fa-user','auth/roles',NULL,'2018-09-18 02:42:56'),(5,2,10,'权限','fa-ban','auth/permissions',NULL,'2018-09-18 02:42:56'),(6,2,11,'菜单','fa-bars','auth/menu',NULL,'2018-09-18 02:42:56'),(7,2,12,'操作日志','fa-history','auth/logs',NULL,'2018-09-18 02:42:56'),(8,0,2,'用户管理','fa-user','/users','2018-06-22 01:57:13','2018-07-12 01:16:02'),(9,0,4,'商品管理','fa-cubes','/products','2018-06-22 06:12:42','2018-09-18 02:42:56'),(10,0,5,'订单管理','fa-rmb','/orders','2018-07-12 01:14:06','2018-09-18 02:42:56'),(11,0,6,'优惠券管理','fa-tags','/coupon_codes','2018-08-10 01:27:37','2018-09-18 02:42:56'),(12,0,3,'类目管理','fa-bars','/categories','2018-09-18 02:42:32','2018-09-18 02:42:56');
 /*!40000 ALTER TABLE `admin_menu` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -103,4 +103,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-09-14  9:05:33
+-- Dump completed on 2018-09-18 11:07:17
